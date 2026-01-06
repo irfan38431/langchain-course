5ef0d069d91a962bc2a5afc9baaac68f941e1914
Irfan Ahmed <i383813@gmail.com>
2026-01-06 13:00:43 +0530

    final job-search agent

--- main.py
+++ main.py
@@ -4,46 +4,48 @@ from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
-
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
-# class Source(BaseModel):
-#     """Schema for a source used by the agent"""
 
-#     url: str = Field(description="The URL of the source")
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
 
 
-# class AgentResponse(BaseModel):
-#     """Schema for agent response with answer and sources"""
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
 
-#     answer: str = Field(description="Thr agent's answer to the query")
-#     sources: List[Source] = Field(
-#         default_factory=list, description="List of sources used to generate the answer"
-#     )
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
 
 
 llm = ChatOllama(temperature=0, model="llama3.1:8b")
-tools = [TavilySearch(
-    max_results=3
-)]
-agent = create_agent(model=llm, tools=tools, system_prompt="""
-    You are a research agent.
-    Use the provided search tool exactly as given.
-    DO NOT add date filters or parameters that are not required.
-    """)
-
+tools = [TavilySearch]
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    system_prompt="""
+                        You are a research agent.
+                        Use the provided search tool exactly as given.
+                        DO NOT add date filters or parameters that are not required.
+    """,
+    response_format=AgentResponse,
+)
 
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke(
         {
-            "messages": [
-                HumanMessage(content="search for 3 job posting for ai engineers using langchain in the bay area and on linkedin and list thier details" )
-            ]
+            "messages": HumanMessage(
+                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
+            )
         }
     )
     print(result)
@@ -51,3 +53,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
